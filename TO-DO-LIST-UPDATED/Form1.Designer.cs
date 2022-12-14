494c15a4ccbc5c4d5fe1484d474f41b9fbfaec81
Ilivieda Emmanuel <Ilivieda1@yahoo.com>
2022-12-14 09:02:21 +0100

    Initial Commit

---
+++ TO-DO-LIST-UPDATED/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿using System.IO;
+namespace TO_DO_LIST_UPDATED
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ADD = new System.Windows.Forms.Button();
+            this.VIEW = new System.Windows.Forms.Button();
+            this.Load = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.combo_Name = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(468, 171);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(330, 42);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // ADD
+            // 
+            this.ADD.Location = new System.Drawing.Point(45, 24);
+            this.ADD.Name = "ADD";
+            this.ADD.Size = new System.Drawing.Size(134, 51);
+            this.ADD.TabIndex = 3;
+            this.ADD.Text = "Add";
+            this.ADD.UseVisualStyleBackColor = true;
+            this.ADD.Click += new System.EventHandler(this.ADD_Click);
+            // 
+            // VIEW
+            // 
+            this.VIEW.Location = new System.Drawing.Point(45, 171);
+            this.VIEW.Name = "VIEW";
+            this.VIEW.Size = new System.Drawing.Size(134, 22);
+            this.VIEW.TabIndex = 4;
+            this.VIEW.Text = "View";
+            this.VIEW.UseVisualStyleBackColor = true;
+            this.VIEW.Click += new System.EventHandler(this.VIEW_Click);
+            // 
+            // Load
+            // 
+            this.Load.Location = new System.Drawing.Point(45, 292);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(134, 23);
+            this.Load.TabIndex = 5;
+            this.Load.Text = "Load";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(45, 397);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(134, 23);
+            this.Save.TabIndex = 6;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // combo_Name
+            // 
+            this.combo_Name.FormattingEnabled = true;
+            this.combo_Name.Location = new System.Drawing.Point(468, 24);
+            this.combo_Name.Name = "combo_Name";
+            this.combo_Name.Size = new System.Drawing.Size(330, 23);
+            this.combo_Name.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.combo_Name);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.VIEW);
+            this.Controls.Add(this.ADD);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button ADD;
+        private System.Windows.Forms.Button VIEW;
+        public System.Windows.Forms.Button Load;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.ComboBox combo_Name;
+    }
+}
---
+++ TO-DO-LIST-UPDATED/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TO_DO_LIST_UPDATED
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> keyValuePairs;
+        string SettingsFileStorageLocation;
+        string SettingsFileName;     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ADD_Click(object sender, EventArgs e)
+        {
+            keyValuePairs = new Dictionary<string, string>();
+            string MyDocuments = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            SettingsFileStorageLocation = Path.Combine(MyDocuments, "IOStream");
+            SettingsFileName = Path.Combine(SettingsFileStorageLocation, "Text1.Txt");
+            if (!Directory.Exists(SettingsFileStorageLocation))
+            {
+                Directory.CreateDirectory(SettingsFileStorageLocation);
+            }
+            if (File.Exists(SettingsFileName))
+            {
+                using (StreamReader sr = new StreamReader(SettingsFileName))
+                {
+                    //VariableName=Value
+                    while (!sr.EndOfStream)
+                    {
+                        string LineFromFile = sr.ReadLine();
+                        string[] NameValuesArray = LineFromFile.Split('=');
+                        if (NameValuesArray.Length == 2)
+                        {
+                            string NameOfVariable = NameValuesArray[0];
+                            string ValueOfVariable = NameValuesArray[1];
+                            if (!keyValuePairs.ContainsKey(NameOfVariable))
+                            {
+                                keyValuePairs.Add(NameOfVariable, ValueOfVariable);
+                            }
+                        }
+                    }
+
+                }
+                foreach (string KeyName in keyValuePairs.Keys)
+                {
+                    combo_Name.Items.Add(KeyName);
+                }
+            }
+        }
+
+        private void VIEW_Click(object sender, EventArgs e)
+        {
+
+           using(StreamWriter sw = new StreamWriter(SettingsFileName))
+            {
+                foreach(string KeyName in keyValuePairs.Keys)
+                {
+                    sw.WriteLine(KeyName + "=" + keyValuePairs[KeyName]);
+                }
+            }
+
+            //string UserEnteredVariableName = combo_Name.Text;
+            //string UserEnteredValue = textBox1.Text;
+
+            //if (keyValuePairs.ContainsKey(UserEnteredVariableName))
+            //{
+            //    keyValuePairs[UserEnteredVariableName] = UserEnteredValue;
+            //}
+            //else
+            //{
+            //    keyValuePairs.Add(UserEnteredVariableName, UserEnteredValue);
+            //    combo_Name.Items.Add(UserEnteredVariableName);
+            //}
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            string UserEnteredVariableName = combo_Name.Text;
+            if (keyValuePairs.ContainsKey(UserEnteredVariableName))
+            {
+                string ValueOfVariable = keyValuePairs[UserEnteredVariableName];
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            string UserEnteredVariableName = combo_Name.Text;
+            string UserEnteredValue = textBox1.Text;
+            if (keyValuePairs.ContainsKey(UserEnteredVariableName))
+            {
+                keyValuePairs[UserEnteredVariableName] = UserEnteredValue;    
+            }
+            else
+            {
+                keyValuePairs.Add(UserEnteredVariableName, UserEnteredValue);
+                combo_Name.Items.Add(UserEnteredVariableName);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
